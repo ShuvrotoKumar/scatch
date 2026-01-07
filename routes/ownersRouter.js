aa428f73955d6f307481075cf6bd29f440b4b05a
ShuvrotoKumar <sdks6409@gmail.com>
2026-01-08 02:43:28 +0600

    config development updated

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -5,4 +5,10 @@ router.get("/", function(req, res){
     res.send("hey");
 });
 
+
+if(process.env.NODE_ENV === "development"){
+router.post("/create", function(req, res){
+    res.send("hey");
+});
+}
 module.exports = router;
